92a13bf2360085abf2c15f9a2b48617023d8dcbe
Efren-Asus <eagp80@gmail.com>
2023-07-15 13:06:08 -0500

    editado archico productsMongo para adaptarlo al formato actual de tipos de datos del modelo y para diversificarlo lo mas posible

--- src/db/productsMongo.js
+++ src/db/productsMongo.js
@@ -2,332 +2,310 @@ const productsMongoData =
 [
     {
         
-        title: "Producto 20",
-        description: "Desde fromulario con socket",
-        code: "abc20",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc20.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc70",
+        price: 30,
+        status: true,
+        stock: 20,
+        category: "Matemáticas",
+        thumbnails: "abc70.png",
+
     },
     {
-        title: "Producto 21",
-        description: "Desde fromulario con socket",
-        code: "abc21",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc21.png"
-    },
-    {
-        
-        title: "Producto 22",
-        description: "Desde fromulario con socket",
-        code: "abc22",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc22.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc71",
+        price: 31,
+        status: true,
+        stock: 21,
+        category: "Electrónica",
+        thumbnails: "abc71.png",
     },
     {
         
-        title: "Producto 23",
-        description: "Desde fromulario con socket",
-        code: "abc23",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc23.png"
-    },
-    {
-       
-        title: "Producto 24",
-        description: "Desde fromulario con socket",
-        code: "abc24",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc24.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc72",
+        price: 32,
+        status: true,
+        stock: 22,
+        category: "Matemáticas",
+        thumbnails: "abc72.png",
+
     },
     {
         
-        title: "Producto 25",
-        description: "Desde fromulario con socket",
-        code: "abc25",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc25.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc73",
+        price: 33,
+        status: true,
+        stock: 23,
+        category: "Electrónica",
+        thumbnails: "abc73.png",
+
+    },
+    {
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc74",
+        price: 34,
+        status: true,
+        stock: 24,
+        category: "Matemáticas",
+        thumbnails: "abc74.png",
     },
     {
         
-        title: "Producto 26",
-        description: "Desde fromulario con socket",
-        code: "abc26",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc26.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc75",
+        price: 35,
+        status: true,
+        stock: 25,
+        category: "Electrónica",
+        thumbnails: "abc75.png",
+
     },
     {
         
-        title: "Producto 27",
-        description: "Desde fromulario con socket",
-        code: "abc27",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc27.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc76",
+        price: 36,
+        status: true,
+        stock: 26,
+        category: "Matemáticas",
+        thumbnails: "abc76.png",
+
+    },
+    {
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc77",
+        price: 37,
+        status: true,
+        stock: 27,
+        category: "Electrónica",
+        thumbnails: "abc77.png",
     },
     {
         
-        title: "Producto 28",
-        description: "Desde fromulario con socket",
-        code: "abc28",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc28.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc78",
+        price: 38,
+        status: true,
+        stock: 28,
+        category: "Matemáticas",
+        thumbnails: "abc78.png",
+
     },
     {
         
-        title: "Producto 29",
-        description: "Desde fromulario con socket",
-        code: "abc29",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc29.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc79",
+        price: 39,
+        status: true,
+        stock: 29,
+        category: "Electrónica",
+        thumbnails: "abc79.png",
+
     },
     {
-       
-        title: "Producto 30",
-        description: "Desde fromulario con socket",
-        code: "abc30",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc30.png"
-    },
-    {
-        
-        title: "Producto 31",
-        description: "Desde fromulario con socket",
-        code: "abc31",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc31.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc80",
+        price: 40,
+        status: true,
+        stock: 30,
+        category: "Matemáticas",
+        thumbnails: "abc80.png",
     },
     {
         
-        title: "Producto 32",
-        description: "Desde fromulario con socket",
-        code: "abc32",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc32.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc81",
+        price: 41,
+        status: true,
+        stock: 31,
+        category: "Electrónica",
+        thumbnails: "abc81.png",
+
     },
     {
         
-        title: "Producto 33",
-        description: "Desde fromulario con socket",
-        code: "abc33",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc33.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc82",
+        price: 42,
+        status: true,
+        stock: 32,
+        category: "Matemáticas",
+        thumbnails: "abc82.png",
+
+    },
+    {
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc83",
+        price: 43,
+        status: true,
+        stock: 33,
+        category: "Electrónica",
+        thumbnails: "abc83.png",
     },
     {
         
-        title: "Producto 34",
-        description: "Desde fromulario con socket",
-        code: "abc34",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc34.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc84",
+        price: 44,
+        status: true,
+        stock: 34,
+        category: "Matemáticas",
+        thumbnails: "abc84.png",
+
     },
     {
         
-        title: "Producto 35",
-        description: "Desde fromulario con socket",
-        code: "abc35",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc35.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc85",
+        price: 45,
+        status: true,
+        stock: 35,
+        category: "Electrónica",
+        thumbnails: "abc85.png",
+
+    },
+    {
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc86",
+        price: 46,
+        status: true,
+        stock: 36,
+        category: "Matemáticas",
+        thumbnails: "abc86.png",
     },
     {
         
-        title: "Producto 36",
-        description: "Desde fromulario con socket",
-        code: "abc36",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc36.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc87",
+        price: 47,
+        status: true,
+        stock: 37,
+        category: "Electrónica",
+        thumbnails: "abc87.png",
+
     },
     {
         
-        title: "Producto 37",
-        description: "Desde fromulario con socket",
-        code: "abc37",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc37.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc88",
+        price: 48,
+        status: true,
+        stock: 38,
+        category: "Matemáticas",
+        thumbnails: "abc88.png",
+
+    },
+    {
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc89",
+        price: 49,
+        status: true,
+        stock: 39,
+        category: "Electrónica",
+        thumbnails: "abc89.png",
     },
     {
         
-        title: "Producto 38",
-        description: "Desde fromulario con socket",
-        code: "abc38",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc38.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc90",
+        price: 50,
+        status: true,
+        stock: 40,
+        category: "Matemáticas",
+        thumbnails: "abc90.png",
+
     },
     {
         
-        title: "Producto 39",
-        description: "Desde fromulario con socket",
-        code: "abc39",
-        price: "10000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc39.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc91",
+        price: 51,
+        status: true,
+        stock: 41,
+        category: "Electrónica",
+        thumbnails: "abc91.png",
+
+    },
+    {
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc92",
+        price: 52,
+        status: true,
+        stock: 42,
+        category: "Electrónica",
+        thumbnails: "abc92.png",
     },
     {
         
-        title: "Producto 40",
-        description: "Desde fromulario con socket",
-        code: "abc40",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc40.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc93",
+        price: 53,
+        status: true,
+        stock: 43,
+        category: "Electrónica",
+        thumbnails: "abc93.png",
+
     },
     {
         
-        title: "Producto 41",
-        description: "Desde fromulario con socket",
-        code: "abc41",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc41.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc94",
+        price: 54,
+        status: true,
+        stock: 44,
+        category: "Electrónica",
+        thumbnails: "abc94.png",
+
     },
     {
-       
-        title: "Producto 42",
-        description: "Desde fromulario con socket",
-        code: "abc42",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc42.png"
-    },
-    {
- 
-        title: "Producto 43",
-        description: "Desde fromulario con socket",
-        code: "abc43",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc43.png"
-    },
-    {
- 
-        title: "Producto 44",
-        description: "Desde fromulario con socket",
-        code: "abc44",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc44.png"
-    },
-    {
-     
-        title: "Producto 45",
-        description: "Desde fromulario con socket",
-        code: "abc45",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc45.png"
-    },
-    {
-      
-        title: "Producto 46",
-        description: "Desde fromulario con socket",
-        code: "abc46",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc46.png"
-    },
-    {
-      
-        title: "Producto 47",
-        description: "Desde fromulario con socket",
-        code: "abc47",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc47.png"
-    },
-    {
-        title: "Producto 48",
-        description: "Desde fromulario con socket",
-        code: "abc48",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc48.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc95",
+        price: 55,
+        status: true,
+        stock: 45,
+        category: "Electrónica",
+        thumbnails: "abc95.png",
     },
     {
         
-        title: "Producto 49",
-        description: "Desde fromulario con socket",
-        code: "abc49",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc49.png"
+        title: "Producto X",
+        description: "Desde app método /products/insertion GET",
+        code: "abc96",
+        price: 56,
+        status: true,
+        stock: 46,
+        category: "Electrónica",
+        thumbnails: "abc96.png",
+
     },
-    {
-       
-        title: "Producto 50",
-        description: "Desde fromulario con socket",
-        code: "abc50",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc50.png"
-    },
-    {
-       
-        title: "Producto 51",
-        description: "Desde fromulario con socket",
-        code: "abc51",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc51.png"
-    },
-    {
-        
-        title: "Producto 52",
-        description: "Desde fromulario con socket",
-        code: "abc52",
-        price: "10.000",
-        stock: "10",
-        category: "A1",
-        thumbnails: "abc52.png"
-    }
 ];
 
 export default productsMongoData;
